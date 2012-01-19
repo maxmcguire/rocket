f25c41a8e7e3dd36e5f358a2f739378c0032bc8f
Max McGuire <max_mcguire@yahoo.com>
2012-01-18 20:39:03 -0800

    Switched to premake based builds

---
+++ premake4.lua
@@ -0,0 +1,66 @@
+solution "Rocket"
+    configurations { "Debug", "Release" }
+    location "build"
+    defines { "_CRT_SECURE_NO_WARNINGS" }
+    vpaths { 
+        ["Header Files"] = "**.h",
+        ["Source Files"] = { "**.cpp", "**.c", "**.asm" },
+        ["Linker Files"] = { "**.def" }
+    }
+
+-- Main library
+project "Rocket"
+    kind "SharedLib"
+    location "build"
+    language "C++"
+    files { "src/*.h", "src/*.c", "src/*.cpp", "src/*.asm", "src/*.def" }
+    includedirs { "include" }
+    links { "AuxLib" }
+    defines { "ROCKET_EXPORTS", "LUA_CORE" }
+
+    configuration "Debug"
+        defines { "DEBUG" }
+        flags { "Symbols" }
+        targetdir "bin/debug"
+
+    configuration "Release"
+        defines { "NDEBUG" }
+        flags { "Optimize" }
+        targetdir "bin/release"   
+     
+-- Auxiliary library     
+project "AuxLib"
+    kind "StaticLib"
+    location "build"
+    language "C++"
+    files { "src/AuxLib/*.h", "src/AuxLib/*.c", "src/AuxLib/*.cpp" }
+    includedirs { "include" }
+
+    configuration "Debug"
+        defines { "DEBUG" }
+        flags { "Symbols" }
+        targetdir "bin/debug"
+
+    configuration "Release"
+        defines { "NDEBUG" }
+        flags { "Optimize" }
+        targetdir "bin/release"         
+
+-- Unit test     
+project "Test"
+    kind "ConsoleApp"
+    location "build"
+    language "C++"
+    files { "src/Test/*.h", "src/Test/*.c", "src/Test/*.cpp" }
+    includedirs { "include" }
+    links { "Rocket" }
+
+    configuration "Debug"
+        defines { "DEBUG" }
+        flags { "Symbols" }
+        targetdir "bin/debug"
+
+    configuration "Release"
+        defines { "NDEBUG" }
+        flags { "Optimize" }     
+        targetdir "bin/release"       
