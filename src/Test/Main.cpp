2127998e3eb5fb8bccae4c2ea4169dcfe93b77a3
Max McGuire <max_mcguire@yahoo.com>
2012-04-15 15:10:02 -0700

    Added the ability to select which unit tests to run using pattern matching

--- src/Test/Main.cpp
+++ src/Test/Main.cpp
@@ -9,6 +9,11 @@
 
 int main(int argc, char* argv[])
 {
-    Test_RunTests();
+    const char* pattern = 0;
+    if (argc > 1)
+    {
+        pattern = argv[1];
+    }
+    Test_RunTests(pattern);
     return 0;
 } 
