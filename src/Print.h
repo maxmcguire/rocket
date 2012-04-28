38760ea53b80ee415f2b3fe6cf615b3724d32452
Max McGuire <max_mcguire@yahoo.com>
2012-04-27 18:03:52 -0700

    Moved printing function disassembly into a separate file

---
+++ src/Print.h
@@ -0,0 +1,18 @@
+/*
+ * RocketVM
+ * Copyright (c) 2011-2012 Max McGuire
+ *
+ * See copyright notice in COPYRIGHT
+ */
+
+#ifndef ROCKETVM_PRINT_H
+#define ROCKETVM_PRINT_H
+
+struct Prototype;
+
+/**
+ * Prints a disassembly of the function to stdout.
+ */
+void PrintFunction(Prototype* prototype);
+
+#endif
