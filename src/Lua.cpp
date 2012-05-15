93468cdc9bc8f85d89a6387398f50105638ed6d9
asraniel <wolf@fryx.ch>
2012-05-15 22:47:47 +0200

    use original Lua.cpp

--- src/Lua.cpp
+++ src/Lua.cpp
@@ -535,14 +535,9 @@ lua_Number lua_tonumber(lua_State *L, int index)
 lua_Integer lua_tointeger(lua_State *L, int index)
 {
     lua_Number  d = lua_tonumber(L, index);
-        
-    #if defined(_MSC_VER)
     lua_Integer i;
     lua_number2integer(i, d);
     return i;
-    #else
-    return (lua_Integer) d;
-    #endif
 }
 
 int lua_toboolean(lua_State* L, int index)
