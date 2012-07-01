1f7533a6e1745be9c8e79d2840c35aa7465eea9d
Max McGuire <max_mcguire@yahoo.com>
2012-07-01 08:38:16 -0700

    Added the ability to specify a Lua file to execute on the command line of the Test project

--- src/Test/Main.cpp
+++ src/Test/Main.cpp
@@ -7,6 +7,15 @@
 
 #include "Test.h"
 
+extern "C"
+{
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+#include <string.h>
+
 int main(int argc, char* argv[])
 {
     const char* pattern = 0;
@@ -14,6 +23,20 @@ int main(int argc, char* argv[])
     {
         pattern = argv[1];
     }
-    Test_RunTests(pattern);
+    if (pattern != NULL && strchr(pattern, '.') != NULL)
+    {
+        lua_State* L = luaL_newstate();
+        luaL_openlibs(L);
+        if (luaL_dofile(L, pattern) != 0)
+        {
+            fputs( lua_tostring(L, -1), stderr );
+            fputc( '\n', stderr );
+        }
+        lua_close(L);
+    }
+    else
+    {
+        Test_RunTests(pattern);
+    }
     return 0;
 } 
